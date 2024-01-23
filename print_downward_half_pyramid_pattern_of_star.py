a5270b8fca1bf1e963f0d261430381315c3b2fdc
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-23 23:59:15 +0800

    Create a code that uses nested loops

--- print_downward_half_pyramid_pattern_of_star.py
+++ print_downward_half_pyramid_pattern_of_star.py
@@ -1,5 +1,9 @@
 # Print a downward Half-Pyramid Pattern of Star
 # Exercise 14
 
-# Creat a pseudocode
+# Create a pseudocode
 
+# Create code the uses nested loops
+for i in range(5):
+    # inner loop for number of columns
+    for j in range(5 - i):
